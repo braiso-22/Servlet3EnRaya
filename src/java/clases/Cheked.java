09865069005b5f302e5832321df397f932862aa3
xabier.barreiroalber <xabier.barreiroalber@EQUIP077238.xunta.local>
2022-01-24 17:16:23 +0100

    LOGICA INICIAL CREADA Y OPTIMIZADA v1

---
+++ src/java/clases/Cheked.java
@@ -0,0 +1,18 @@
+
+package clases;
+import java. util.Scanner;
+import clases.juego;
+
+public class Cheked {
+    
+    
+    public static void main(String[] args) {
+        boolean result;
+
+                
+        
+        
+        
+    }
+    
+}
