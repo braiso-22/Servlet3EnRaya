3b98abde1f778fbc846ab8625d5bd0ff7d7aad3d
xabier.barreiroalber <xabier.barreiroalber@EQUIP077238.xunta.local>
2022-01-24 16:44:15 +0100

    LOGICA INICIAL CREADA

--- src/java/clases/juego.java
+++ src/java/clases/juego.java
@@ -9,5 +9,122 @@ package clases;
  * @author brais.fernandezvazqu
  */
 public class juego {
+    int a1;
+    int a2;
+    int a3;
+    int b1;
+    int b2;
+    int b3;
+    int c1;
+    int c2;
+    int c3;
+    
+    public boolean jugador1win(){
+         
+        boolean traya = false;
+       
+        
+        // FILA SUPERIOR COMPROBACIÓN DE RESULTADOS
+        if(a1 == 1){
+            if(a2 == 1 && a3 == 1){
+               traya = true; 
+            }
+            if(b1 == 1 && c1 == 1){
+                traya = true;
+            }
+            if(b2 == 1 && c3 == 1){
+                traya = true;
+            }  
+        }
+        if(a2 == 1){
+            if(a1 == 1 && a3 == 1){
+               traya = true; 
+            }
+            if(b2 == 1 && c2 == 1){
+                traya = true;
+            }  
+        }
+        if(a3 == 1){
+            if(a1 == 1 && a2 == 1){
+               traya = true; 
+            }
+            if(b3 == 1 && c3 == 1){
+                traya = true;
+            }
+            if(b2 == 1 && c1 == 1){
+                traya = true;
+            }
+        }
+        //__________________________________________________________________
+        // FILA MEDIO COMPROBACIÓN DE RESULTADOS
+        if(b1 == 1){
+            if(b2 == 1 && b3 == 1){
+               traya = true; 
+            }
+            if(a1 == 1 && c1 == 1){
+                traya = true;
+            } 
+        }
+        if(b2 == 1){
+            if(a1 == 1 && c3 == 1){
+               traya = true; 
+            }
+            if(b1 == 1 && b3 == 1){
+                traya = true;
+            }
+            if(c1 == 1 && a3 == 1){
+                traya = true;
+            }
+            if(a2 == 1 && c2 == 1){
+                traya = true;
+            }
+        }
+        if(b3 == 1){
+            if(a3 == 1 && c3 == 1){
+               traya = true; 
+            }
+            if(b1 == 1 && b2 == 1){
+                traya = true;
+            }
+        }
+        
+        //_________________________________________________________________
+        // FILA INFERIOR COMPROBACIÓN DE RESULTADOS
+        if(c1 == 1){
+            if(c2 == 1 && c3 == 1){
+               traya = true; 
+            }
+            if(a1 == 1 && b1 == 1){
+                traya = true;
+            }
+            if(b2 == 1 && a3 == 1){
+                traya = true;
+            }  
+        }
+        if(c2 == 1){
+            if(c1 == 1 && c3 == 1){
+               traya = true; 
+            }
+            if(b2 == 1 && a2 == 1){
+                traya = true;
+            }  
+        }
+        if(c3 == 1){
+            if(c1 == 1 && c2 == 1){
+               traya = true; 
+            }
+            if(b3 == 1 && a3 == 1){
+                traya = true;
+            }
+            if(b2 == 1 && a1 == 1){
+                traya = true;
+            }
+        }
+
+    return traya;
+    }
+    
+    
+    
     
 }
