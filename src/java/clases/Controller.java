67d470da3030c8398014bfb45ca8e2d823ad5b38
braiso-22 <braisfv22@gmail.com>
2022-01-27 18:49:03 +0100

    arreglado fallo

--- src/java/clases/Controller.java
+++ src/java/clases/Controller.java
@@ -17,22 +17,23 @@ public class Controller {
     public String cambiaValores(String cadenaFinal) {
         // int[] valores = gestorXml.getValores() == [0,0,0,0,0,0,0,0,0]
         String subString = cadenaFinal;
+        cadenaFinal="";
         /*
         for(int i =0;i<valores.length;i++){
         switch(valores[i]){
         case 0:
         
             String[] subStringArray = subString.split("</td>\n", 2);
-            cadenaFinal = subStringArray[0] + "</td>\n";
+            cadenaFinal += subStringArray[0] + "</td>\n";
             subString =subStringArray[1];
         
         case 1:
-        subStringArray[1] = subStringArray[1].replaceFirst("\u200E", "X");
+        subString = subString.replaceFirst("\u200E", "X");
         case 2:
-        subStringArray[1] = subStringArray[1].replaceFirst("\u200E", "O");
+        subString = subString.replaceFirst("\u200E", "O");
         default:
         }
-        cadenaFinal+=subStringArray[1];
+        cadenaFinal+=subString;
         }
          */
 
