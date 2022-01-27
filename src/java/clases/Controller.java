7564c671259d4a46f94b0b346cabb8974b385208
braiso-22 <braisfv22@gmail.com>
2022-01-27 18:43:12 +0100

    creado el reemplazador de caracteres

--- src/java/clases/Controller.java
+++ src/java/clases/Controller.java
@@ -14,13 +14,29 @@ public class Controller {
         //gestorXml.pasarDatos(casilla, parOImpar)
     }
 
-    public void cambiaValores(String cadenaFinal) {
+    public String cambiaValores(String cadenaFinal) {
         // int[] valores = gestorXml.getValores() == [0,0,0,0,0,0,0,0,0]
+        String subString = cadenaFinal;
         /*
         for(int i =0;i<valores.length;i++){
-            
+        switch(valores[i]){
+        case 0:
+        
+            String[] subStringArray = subString.split("</td>\n", 2);
+            cadenaFinal = subStringArray[0] + "</td>\n";
+            subString =subStringArray[1];
+        
+        case 1:
+        subStringArray[1] = subStringArray[1].replaceFirst("\u200E", "X");
+        case 2:
+        subStringArray[1] = subStringArray[1].replaceFirst("\u200E", "O");
+        default:
         }
-        */
-        //cadenaFinal.
+        cadenaFinal+=subStringArray[1];
+        }
+         */
+
+        return cadenaFinal;
+
     }
 }
